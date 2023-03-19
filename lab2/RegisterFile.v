0af38d4201a7d51792c863ab4e997d8723f32381
hyunjunekim02 <hyunjunekim@postech.ac.kr>
2023-03-19 17:07:51 +0900

    Register file completed

--- lab2/RegisterFile.v
+++ lab2/RegisterFile.v
@@ -6,14 +6,26 @@ module RegisterFile(input	reset,
                     input [31:0] rd_din,      // input data for rd
                     input write_enable,          // RegWrite signal
                     output [31:0] rs1_dout,   // output of rs 1
-                    output [31:0] rs2_dout);  // output of rs 2
+                    output [31:0] rs2_dout    // output of rs 2
+                    output x_17_is_10);       // output of whether x_17 is equals to 10
   integer i;
   // Register file
   reg [31:0] rf[0:31];
 
   // TODO
   // Asynchronously read register file
+  assign rs1_dout = rf[rs1];
+  assign rs2_dout = rf[rs2];
+  assign x17_is_10 = (rf[17] == 10);
+
   // Synchronously write data to the register file
+  always @(posedge clk) begin
+    if(write_enable) begin
+      if(rd!=0) begin
+        rf[rd] <= rd_din;
+      end
+    end
+  end
 
   // Initialize register file (do not touch)
   always @(posedge clk) begin
@@ -24,4 +36,5 @@ module RegisterFile(input	reset,
       rf[2] = 32'h2ffc; // stack pointer
     end
   end
+  
 endmodule
