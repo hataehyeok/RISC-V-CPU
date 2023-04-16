e20a4c16971a98057ec6a59b176932fe2f06ded0
hyunjunekim02 <hyunjunekim@postech.ac.kr>
2023-04-17 02:16:56 +0900

    PC module modified

--- Lab3/PC.v
+++ Lab3/PC.v
@@ -2,19 +2,15 @@ module PC (input reset,
             input clk,
             input pc_control,
             input [31:0] next_pc,
-            output [31:0] current_pc);
-  
-  reg [31:0] pc;
+            output reg [31:0] current_pc);
 
-  assign current_pc = pc;
-  
   always @(posedge clk) begin
     if(reset) begin
-      pc <= 32'b0;
+      current_pc <= 32'b0;
     end
     else begin
         if(pc_control) begin
-            pc <= next_pc;
+            current_pc <= next_pc;
         end
     end
   end
