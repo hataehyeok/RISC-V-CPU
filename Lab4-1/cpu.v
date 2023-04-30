d247aea2dbcb48a587aab39578e2525aae710e8e
hyunjunekim02 <hyunjunekim@postech.ac.kr>
2023-05-01 01:31:12 +0900

    Some modifications

--- Lab4-1/cpu.v
+++ Lab4-1/cpu.v
@@ -12,27 +12,54 @@ module CPU(input reset,       // positive reset signal
            input clk,         // clock signal
            output is_halted); // Whehther to finish simulation
   /***** Wire declarations *****/
+
+  // ---------- Wire of PC ----------
+  wire [31:0] next_pc;
+  wire [31:0] current_pc;
+  wire pc_control;
+  // ---------- Wire of InstMemory ----------
+  wire [31:0] inst_dout;
+  // ---------- Wire of Registers ----------
+  wire [4:0] rs1;
+  wire [4:0] rs2;
+  wire [4:0] rd;
+  wire [31:0] rd_din;
+  wire [31:0] rs1_dout;
+  wire [31:0] rs2_dout;
+  //---------- Wire of ControlUnit ----------
+
+  //---------- Wire of ImmediateGenerator ----------
+  wire [31:0] imm_gen_out;
+  //---------- Wire of ALUControlUnit ----------
+  wire [2:0] alu_op;
+  //---------- Wire of ALU ----------
+  wire [31:0] alu_in_1;
+  wire [31:0] alu_in_2;
+  wire [31:0] alu_result;
+  //---------- Wire of DataMemory ----------
+  wire [31:0] data_dout;
+
   /***** Register declarations *****/
   // You need to modify the width of registers
   // In addition, 
   // 1. You might need other pipeline registers that are not described below
   // 2. You might not need registers described below
   /***** IF/ID pipeline registers *****/
-  reg IF_ID_inst;           // will be used in ID stage
+  reg [31:0] IF_ID_inst;           // will be used in ID stage
   /***** ID/EX pipeline registers *****/
   // From the control unit
-  reg ID_EX_alu_op;         // will be used in EX stage
+  reg [1:0] ID_EX_alu_op;         // will be used in EX stage
   reg ID_EX_alu_src;        // will be used in EX stage
   reg ID_EX_mem_write;      // will be used in MEM stage
   reg ID_EX_mem_read;       // will be used in MEM stage
   reg ID_EX_mem_to_reg;     // will be used in WB stage
   reg ID_EX_reg_write;      // will be used in WB stage
   // From others
-  reg ID_EX_rs1_data;
-  reg ID_EX_rs2_data;
-  reg ID_EX_imm;
-  reg ID_EX_ALU_ctrl_unit_input;
-  reg ID_EX_rd;
+  reg [31:0] ID_EX_rs1_data;
+  reg [31:0] ID_EX_rs2_data;
+  reg [31:0] ID_EX_imm;
+  reg [31:0] ID_EX_ALU_ctrl_unit_input;
+  reg [4:0] ID_EX_rd;
 
   /***** EX/MEM pipeline registers *****/
   // From the control unit
@@ -42,33 +69,34 @@ module CPU(input reset,       // positive reset signal
   reg EX_MEM_mem_to_reg;    // will be used in WB stage
   reg EX_MEM_reg_write;     // will be used in WB stage
   // From others
-  reg EX_MEM_alu_out;
-  reg EX_MEM_dmem_data;
-  reg EX_MEM_rd;
+  reg [31:0] EX_MEM_alu_out;
+  reg [31:0] EX_MEM_dmem_data;
+  reg [4:0] EX_MEM_rd;
 
   /***** MEM/WB pipeline registers *****/
   // From the control unit
   reg MEM_WB_mem_to_reg;    // will be used in WB stage
   reg MEM_WB_reg_write;     // will be used in WB stage
   // From others
-  reg MEM_WB_mem_to_reg_src_1;
-  reg MEM_WB_mem_to_reg_src_2;
+  reg [31:0] MEM_WB_mem_to_reg_src_1;
+  reg [31:0] MEM_WB_mem_to_reg_src_2;
 
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
   PC pc(
-    .reset(),       // input (Use reset to initialize PC. Initial value must be 0)
-    .clk(),         // input
-    .next_pc(),     // input
-    .current_pc()   // output
+    .reset(reset),       // input (Use reset to initialize PC. Initial value must be 0)
+    .clk(clk),         // input
+    .pc_control(pc_control)
+    .next_pc(next_pc),     // input
+    .current_pc(current_pc)   // output
   );
   
   // ---------- Instruction Memory ----------
   InstMemory imem(
-    .reset(),   // input
-    .clk(),     // input
-    .addr(),    // input
-    .dout()     // output
+    .reset(reset),   // input
+    .clk(clk),     // input
+    .addr(current_pc),    // input
+    .dout(inst_dout)     // output
   );
 
   // Update IF/ID pipeline registers here
@@ -81,15 +109,15 @@ module CPU(input reset,       // positive reset signal
 
   // ---------- Register File ----------
   RegisterFile reg_file (
-    .reset (),        // input
-    .clk (),          // input
-    .rs1 (),          // input
-    .rs2 (),          // input
-    .rd (),           // input
-    .rd_din (),       // input
-    .write_enable (),    // input
-    .rs1_dout (),     // output
-    .rs2_dout ()      // output
+    .reset (reset),        // input
+    .clk (clk),          // input
+    .rs1 (rs1),          // input
+    .rs2 (rs2),          // input
+    .rd (rd),           // input
+    .rd_din (rd_din),       // input
+    .write_enable (MEM_WB_reg_write),    // input
+    .rs1_dout (rs1_dout),     // output
+    .rs2_dout (rs2_dout)      // output
   );
 
 
@@ -108,8 +136,8 @@ module CPU(input reset,       // positive reset signal
 
   // ---------- Immediate Generator ----------
   ImmediateGenerator imm_gen(
-    .part_of_inst(),  // input
-    .imm_gen_out()    // output
+    .part_of_inst(IF_ID_inst),  // input
+    .imm_gen_out(imm_gen_out)    // output
   );
 
   // Update ID/EX pipeline registers here
@@ -128,10 +156,10 @@ module CPU(input reset,       // positive reset signal
 
   // ---------- ALU ----------
   ALU alu (
-    .alu_op(),      // input
-    .alu_in_1(),    // input  
-    .alu_in_2(),    // input
-    .alu_result(),  // output
+    .alu_op(alu_op),      // input
+    .alu_in_1(alu_in_1),    // input  
+    .alu_in_2(alu_in_2),    // input
+    .alu_result(alu_result),  // output
     .alu_zero()     // output
   );
 
@@ -145,13 +173,13 @@ module CPU(input reset,       // positive reset signal
 
   // ---------- Data Memory ----------
   DataMemory dmem(
-    .reset (),      // input
-    .clk (),        // input
-    .addr (),       // input
-    .din (),        // input
-    .mem_read (),   // input
-    .mem_write (),  // input
-    .dout ()        // output
+    .reset (reset),      // input
+    .clk (clk),        // input
+    .addr (EX_MEM_alu_out),       // input
+    .din (EX_MEM_dmem_data),        // input
+    .mem_read (EX_MEM_mem_read),   // input
+    .mem_write (EX_MEM_mem_write),  // input
+    .dout (data_dout)        // output
   );
 
   // Update MEM/WB pipeline registers here
