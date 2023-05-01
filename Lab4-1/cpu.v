9f81d75024ae88c591e73d0ee6a364000bfa5005
hataehyeok <105369662+hataehyeok@users.noreply.github.com>
2023-05-02 01:00:54 +0900

    Edit formatting error

--- Lab4-1/cpu.v
+++ Lab4-1/cpu.v
@@ -357,12 +357,12 @@ module CPU(input reset,       // positive reset signal
     .rd(rd),
     .EX_MEM_rd(EX_MEM_rd),
     .is_ecall(is_ecall),
-    .rd_din(rd_din)
-    .rs1_dout(rs1_dout)
-    .rs2_dout(rs2_dout)
-    .EX_MEM_alu_out(EX_MEM_alu_out)
+    .rd_din(rd_din),
+    .rs1_dout(rs1_dout),
+    .rs2_dout(rs2_dout),
+    .EX_MEM_alu_out(EX_MEM_alu_out),
     .f_rs1_dout(f_rs1_dout),
-    .f_rs2_dout(f_rs2_dout)
+    .f_rs2_dout(f_rs2_dout),
   );
   // // 그 밑에 따까리 Mux 두개
   // threeSigMUX mux_for_rs1_dout(
