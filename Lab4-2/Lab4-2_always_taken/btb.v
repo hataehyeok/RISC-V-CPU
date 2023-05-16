3f99055bbd9839a96a7569b01d20006a73e7315b
hyunjunekim02 <hyunjunekim@postech.ac.kr>
2023-05-16 17:40:02 +0900

    always taken btb.v modified for report

--- Lab4-2/Lab4-2_always_taken/btb.v
+++ Lab4-2/Lab4-2_always_taken/btb.v
@@ -1,96 +1,92 @@
+//BTB unit
 module BTB(input [31:0] pc,
-            input reset,
-            input clk,
-            input [31:0] IF_ID_pc,
-            input is_jal, // ID_EX
-            input is_jalr, // ID_EX
-            input branch, // ID_EX
-            input bcond,
-            input [31:0] write_pc, // ID_EX
-            input [31:0] pc_plus_imm,
-            input [31:0] reg_plus_imm,
-            output reg [31:0] n_pc
-            // output reg miss_prediction
-            );
+           input reset,
+           input clk,
+           input [31:0] IF_ID_pc,
+           input alu_bcond,
+           input [31:0] write_pc,
+           input is_jal,
+           input is_jalr,
+           input branch,
+           input [31:0] pc_plus_imm,
+           input [31:0] reg_plus_imm,
+           output reg [31:0] nPC
+           );
 
-    // wire declaration
-    wire [24:0] tag;
-    wire [4:0] index;
+  //wire
+  wire [31:0] tag;
+  wire [4:0] index;
+  wire [4:0] write_idx;
+  wire [31:0] write_tag;
 
-    wire [24:0] write_tag;
-    wire [4:0] write_index;
+  //reg
+  reg [5:0] idx;
+  reg [31:0] tag_table[0:31];
+  reg [31:0] btb[0:31];
 
-    // reg declaration
-    reg [5:0] i;
-    reg [31:0] btb[0:31];
-    reg [24:0] tag_table[0:31];
+  // assign
+  assign tag = pc[31:7];
+  assign index = pc[6:2];
+  assign write_tag = write_pc[31:7];
+  assign write_idx = write_pc[6:2];
 
-    // assignment
-    assign tag=pc[31:7];
-    assign index=pc[6:2];
-    
-    assign write_tag=write_pc[31:7];
-    assign write_index=write_pc[6:2];
-
-    always @(*) begin
-        if(tag_table[index]==tag) begin
-            n_pc=btb[index];
-        end
-        else begin
-            n_pc=pc+4;
-        end
+  always @(*) begin
+    if(tag_table[index] == tag) begin
+      nPC = btb[index];
     end
-
-    always @(*) begin
-        if(is_jal|branch) begin
-            if((tag_table[write_index]!=write_tag)|(btb[write_index]!=pc_plus_imm)) begin
-                tag_table[write_index]=write_tag;
-                btb[write_index]=pc_plus_imm;
-            end
-        end
-        else if(is_jalr) begin
-            if((tag_table[write_index]!=write_tag)|(btb[write_index]!=reg_plus_imm)) begin
-                tag_table[write_index]=write_tag;
-                btb[write_index]=reg_plus_imm;
-            end
-        end
+    else begin
+      nPC = pc + 4;
     end
+  end
 
-    always @(posedge clk) begin
-        if (reset) begin
-            for(i=0; i < 32;i=i+1) begin
-                btb[i] = 0;
-                tag_table[i] = -1; // tag를 0으로 하면 초반에 모든 pc가 전부 tag가 일치해서 n_pc를 0으로 넣어버림.
-            end
-        end
+  always @(*) begin
+    if (is_jal | branch) begin
+      if ((tag_table[write_idx] != write_tag) | (btb[write_idx] != pc_plus_imm)) begin
+        tag_table[write_idx] = write_tag;
+        btb[write_idx] = pc_plus_imm;
+      end
     end
+    else if (is_jalr) begin
+      if ((tag_table[write_idx] != write_tag) | (btb[write_idx] != reg_plus_imm)) begin
+        tag_table[write_idx] = write_tag;
+        btb[write_idx] = reg_plus_imm;
+      end
+    end
+  end
 
+  always @(posedge clk) begin
+      if (reset) begin
+          for(idx = 0; idx < 32; idc = idx + 1) begin
+              btb[idx] = 0;
+              tag_table[idx] = -1;
+          end
+      end
+  end
 
 endmodule
 
-module MissPredictionDetector(
-    input [31:0] IF_ID_pc,
-    input ID_EX_is_jal, // ID_EX
-    input ID_EX_is_jalr, // ID_EX
-    input ID_EX_branch, // ID_EX
-    input ID_EX_bcond,
-    input [31:0] ID_EX_pc,
-    input [31:0] pc_plus_imm,
-    input [31:0] reg_plus_imm,
-    output reg is_miss_pred);
+//Predicting Misses
+module MissPredDetector(input [31:0] IF_ID_pc,
+                        input ID_EX_is_jal,
+                        input ID_EX_is_jalr,
+                        input ID_EX_branch,
+                        input ID_EX_bcond,
+                        input [31:0] ID_EX_pc,
+                        input [31:0] pc_plus_imm,
+                        input [31:0] reg_plus_imm,
+                        output reg is_miss_pred);
 
-    always @(*) begin
-        if((ID_EX_is_jal|(ID_EX_branch&ID_EX_bcond))&(IF_ID_pc!=pc_plus_imm)) begin
-            is_miss_pred=1;
-        end
-        else if((ID_EX_is_jalr)&(IF_ID_pc!=reg_plus_imm)) begin
-            is_miss_pred=1;
-        end
-        else if(ID_EX_branch&(!ID_EX_bcond)&(IF_ID_pc!=ID_EX_pc+4)) begin
-            is_miss_pred=1;
-        end
-        else begin
-            is_miss_pred=0;
-        end 
+  wire is_jal_or_taken = (ID_EX_is_jal | (ID_EX_branch & ID_EX_bcond)) & (IF_ID_pc != pc_plus_imm);
+  wire is_jalr_or_taken  = (ID_EX_is_jalr) & (IF_ID_pc != reg_plus_imm);
+  wire is_branch  = (IF_ID_pc != ID_EX_pc+4) & (ID_EX_branch & !ID_EX_bcond);
+
+  always @(*) begin
+    if(is_jal_or_taken | is_jalr_or_taken | is_branch) begin
+      is_miss_pred=1;
     end
+    else begin
+      is_miss_pred=0;
+    end 
+  end
+
 endmodule
