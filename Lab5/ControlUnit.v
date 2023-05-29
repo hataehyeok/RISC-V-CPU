8edb558744d69dc29b47a5fe25cf7e2e978dc957
hyunjunekim02 <hyunjunekim@postech.ac.kr>
2023-05-29 18:23:35 +0900

    controlUnit changed

--- Lab5/ControlUnit.v
+++ Lab5/ControlUnit.v
@@ -1,17 +1,17 @@
 `include "opcodes.v"
 
-module ControlUnit(input [6:0] part_of_inst,  // input : opcode
-                    output is_jal,        // output 
-                    output is_jalr,       // output
-                    output branch,        // output
-                    output mem_read,      // output
-                    output mem_to_reg,    // output
-                    output mem_write,     // output
-                    output alu_src,       // output
-                    output reg_write,     // output : RegWrite
-                    output pc_to_reg,     // output
-                    output reg [1:0] alu_op,
-                    output is_ecall);      // output (ecall inst)
+module ControlUnit( input [6:0] part_of_inst,
+                    output mem_read,
+                    output mem_to_reg,
+                    output mem_write,
+                    output alu_src,
+                    output reg_write,
+                    output [1:0] alu_op,
+                    output is_jal,
+                    output is_jalr,
+                    output branch,
+                    output pc_to_reg,
+                    output is_ecall);
 
   assign is_jal=(part_of_inst==`JAL);
   assign is_jalr=(part_of_inst==`JALR);
@@ -22,20 +22,18 @@ module ControlUnit(input [6:0] part_of_inst,  // input : opcode
   assign alu_src=(part_of_inst==`ARITHMETIC_IMM || part_of_inst==`LOAD || part_of_inst==`JALR || part_of_inst==`STORE);
   assign reg_write=(part_of_inst!=`STORE && part_of_inst!=`BRANCH);
   assign pc_to_reg=(part_of_inst==`JAL || part_of_inst==`JALR);
-
   assign is_ecall=(part_of_inst==`ECALL);
 
   always @(*) begin
       if(part_of_inst==`LOAD||part_of_inst==`STORE||part_of_inst==`JAL||part_of_inst==`JALR) begin
-          alu_op = 2'b00; // 무조건 add
+          alu_op = 2'b00;
       end
       else if(part_of_inst==`BRANCH) begin
-          alu_op = 2'b01; // 무조건 sub
+          alu_op = 2'b01;
       end
       else if(part_of_inst==`ARITHMETIC||part_of_inst==`ARITHMETIC_IMM) begin
-          alu_op = 2'b10; // func3와 func7 참조
+          alu_op = 2'b10;
       end
   end
-
 
 endmodule
